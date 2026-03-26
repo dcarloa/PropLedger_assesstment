de08e481e6b0e6275af6f9db5a5b5f043fb06a9f
dcarloa <dcarloabad@gmail.com>
2026-03-25 22:22:08 -0600

    Update students controller

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -41,3 +41,39 @@ module.exports = {
     handleStudentStatus,
     handleUpdateStudent,
 };
+
+// ## Lets compare with staffs controller to find the pattern and then we can apply the same pattern to students controller.
+
+// const handleGetAllStaffs = asyncHandler(async (req, res) => {
+//     const { userId, roleId, name } = req.query;
+//     const staffs = await processGetAllStaffs({ userId, roleId, name });
+//     res.json({ staffs });
+// });
+
+// const handleGetStaff = asyncHandler(async (req, res) => {
+//     const { id } = req.params;
+//     const staff = await processGetStaff(id);
+//     res.json(staff);
+
+// });
+
+// const handleReviewStaffStatus = asyncHandler(async (req, res) => {
+//     const payload = req.body;
+//     const { id: userId } = req.params;
+//     const { id: reviewerId } = req.user;
+//     const message = await processReviewStaffStatus({ ...payload, userId, reviewerId });
+//     res.json(message);
+// });
+
+// const handleAddStaff = asyncHandler(async (req, res) => {
+//     const payload = req.body;
+//     const message = await processAddStaff(payload);
+//     res.json(message);
+// });
+
+// const handleUpdateStaff = asyncHandler(async (req, res) => {
+//     const { id: userId } = req.params;
+//     const payload = req.body;
+//     const message = await processUpdateStaff({ ...payload, userId });
+//     res.json(message);
+// });
